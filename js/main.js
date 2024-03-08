129cb8404b781ae7fb49c4f297b1dc6db4e2ad84
NobbyDK <nobbydharma21@gmail.com>
2024-03-09 02:44:04 +0700

    Update Mobile friendly link

--- js/main.js
+++ js/main.js
@@ -10,3 +10,18 @@ document.addEventListener('DOMContentLoaded', function() {
         navUl.classList.toggle('show');
     });
 });
+
+// Ambil elemen teks welcome
+var welcomeText = document.querySelector('.welcome-text');
+
+// Fungsi untuk menentukan apakah teks welcome harus ditampilkan atau disembunyikan
+function toggleWelcomeText() {
+  if (window.scrollY > 50) {
+    welcomeText.style.display = 'none'; // Jika pengguna menggulir halaman, sembunyikan teks welcome
+  } else {
+    welcomeText.style.display = 'block'; // Jika pengguna berada di bagian atas halaman, tampilkan teks welcome
+  }
+}
+
+// Tambahkan event listener untuk mengaktifkan fungsi ketika halaman digulir
+window.addEventListener('scroll', toggleWelcomeText);
